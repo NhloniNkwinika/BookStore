c6985ca35251559e3a49d978a10137840a9a3873
NhloniNkwinika <hloninkwinika@gmail.com>
2024-06-10 17:12:47 +0200

    cook books sections added

--- script.js
+++ script.js
@@ -22,6 +22,13 @@ function showSignUpForm() {
     document.getElementById('login-form').style.display = 'none';
     document.getElementById('signup-form').style.display = 'block';
 }
-
+function toggleContent(id) {
+    let content = document.getElementById(id);
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
 
 
