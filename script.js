870a50aa1b6cee1ea43b35b82190098744f2c82b
NhloniNkwinika <hloninkwinika@gmail.com>
2024-06-10 13:52:13 +0200

    LogIn/SignUp page created

--- script.js
+++ script.js
@@ -9,6 +9,19 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    showLoginForm();
+});
+
+function showLoginForm() {
+    document.getElementById('login-form').style.display = 'block';
+    document.getElementById('signup-form').style.display = 'none';
+}
+
+function showSignUpForm() {
+    document.getElementById('login-form').style.display = 'none';
+    document.getElementById('signup-form').style.display = 'block';
+}
 
 
 
